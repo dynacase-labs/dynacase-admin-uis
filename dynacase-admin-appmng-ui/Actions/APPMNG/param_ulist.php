d3d4bc0f815f77cfcccf9bf69cae4555cc6a8835
Charles Bonnissent <charles.bonnissent@anakeen.com>
2013-02-26 15:34:10 +0100

    Correct user search (closes #3685)

--- dynacase-admin-appmng-ui/Actions/APPMNG/param_ulist.php
+++ dynacase-admin-appmng-ui/Actions/APPMNG/param_ulist.php
@@ -18,9 +18,7 @@
 //  -----------------------------------
 function param_ulist(Action & $action)
 {
-    // -----------------------------------
-    $userid = $action->getArgument("userid");
-    
+
     $jslinks = array(
         array(
             "src" => $action->parent->getJsLink("lib/jquery/jquery.js")
@@ -79,24 +77,40 @@ function param_ulist(Action & $action)
     return;
 }
 
-function appmngGetUsers(Action & $action)
+/**
+ * Get user list
+ *
+ * @param Action $action
+ */
+function appmngGetUsers(Action &$action)
 {
-    $filterName = $action->getArgument("filterName");
-    
-    $u = new Account();
-    $list = $u->GetUserList("TABLE", 0, 25, $filterName);
-    $data = array();
-    // select the wanted user
-    foreach ($list as $v) {
+    $usage = new ActionUsage($action);
+
+    $filterName = $usage->addOptionalParameter("filterName", "filterName");
+
+    $usage->verify(true);
+
+    $data = [];
+
+    $search = new SearchDoc("", "IUSER");
+    $search->setObjectReturn(true);
+    $search->addFilter("title ~* '%s' or us_login ~* '%s'", $filterName, $filterName);
+    $search->setSlice(25);
+
+    foreach($search->getDocumentList() as $currentUser) {
+        /* @var $currentUser Doc */
         $data[] = array(
-            "label" => trim(sprintf("%s %s", $v["lastname"], $v["firstname"])) ,
-            "value" => $v["id"]
+            "label" => trim(sprintf("%s (%s)", $currentUser->getTitle(), $currentUser->getRawValue("us_login"))) ,
+            "value" => $currentUser->getRawValue("us_whatid")
         );
     }
-    if ((count($data) == 0) && ($filterName != '')) $data[] = array(
-        "label" => sprintf(_("no account match '%s'") , $filterName) ,
-        "value" => 0
-    );
+    if ((count($data) == 0) && ($filterName != '')) {
+        $data[] = array(
+            "label" => sprintf(_("no account match '%s'") , $filterName) ,
+            "value" => 0
+        );
+    }
+
     $action->lay->template = json_encode($data);
     $action->lay->noparse = true;
     
